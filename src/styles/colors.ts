2f0448e7c4653eaecab94c66b1dd2442d8e7835e
rik <rikrocks4@gmail.com>
2022-06-19 21:25:34 +0530

    Setup project

---
+++ src/styles/colors.ts
@@ -0,0 +1,17 @@
+const colors = {
+    mainPurple: '#635FC7',
+    mainPurpleHover: '#A8A4FF',
+    black: "#000112",
+    veryDarkGrey: "#20212C",
+    darkGrey: "#2B2C37",
+    lines: "#3E3F4E",
+    mediumGrey: "#828FA3",
+    linesLight: "#E4EBFA",
+    lightGrey: "#F4F7FD",
+    white: "#FFFFFF",
+    red: "#EA5555",
+    redHover:"#FF9898"
+
+}
+
+export default colors
---
+++ src/styles/text.ts
@@ -0,0 +1,14 @@
+const text = {
+    fontWeight: {
+        bold: 700,
+        medium: 500
+    },
+    fontSize: {
+        xl: '2.5rem',
+        l: '1.8rem',
+        m: '1.5rem',
+        s: '1.2rem'
+    }
+}
+
+export default text
